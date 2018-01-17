fc0fd5adfa5bb14a0a59a5d9de69642dbdc25bb3
Billy Vong <billy@sentry.io>
2018-01-16 16:06:44 -0800

    feat(ui): Change "Saved Searches" to use `Panel`

--- tests/js/setup.js
+++ tests/js/setup.js
@@ -77,6 +77,27 @@ window.TestStubs = {
     };
   },
 
+  Searches: params => [
+    {
+      name: 'Needs Triage',
+      dateCreated: '2017-11-14T02:22:58.026Z',
+      isUserDefault: false,
+      isPrivate: false,
+      query: 'is:unresolved is:unassigned',
+      id: '2',
+      isDefault: true,
+    },
+    {
+      name: 'Unresolved Issues',
+      dateCreated: '2017-11-14T02:22:58.022Z',
+      isUserDefault: true,
+      isPrivate: false,
+      query: 'is:unresolved',
+      id: '1',
+      isDefault: false,
+    },
+  ],
+
   Team: params => {
     return {
       id: '1',
